650ba41217d6f333b6e5fbfd252c201fea2336b7
Cheprichate <petrovich15071998@mail.ru>
2023-10-10 11:41:58 +0400

    Добавьте файлы проекта.

---
+++ Fibonachchi.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int num;
+int f1 = 0, f2 = 1, f3 = 1;
+int a = 1;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите номер числа: ";
+    std::cin >> num;
+    do {
+        a++;
+        std::cout << f1 << "  ";
+        f2 = f3;
+        f3 = f1;
+        f1 = f2 + f3;
+    } while (a <= num);
+}
